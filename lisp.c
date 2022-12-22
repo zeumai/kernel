45270e6bbc6450825a4743c3d4972819fbf1c529
zeumai <zeumai@gmail.com>
2022-12-21 21:09:50 -0700

    Make functions applicative as appropriate

--- lisp.c
+++ lisp.c
@@ -8,8 +8,8 @@ int main(int argc, char *argv[]) {
     dict_set(e->bindings, (Symbol *)symbol("lambda"), primitive(&prm_lambda));
     dict_set(e->bindings, (Symbol *)symbol("quote"), primitive(&prm_quote));
     dict_set(e->bindings, (Symbol *)symbol("eq?"), applicative(primitive(&prm_eq)));
-    dict_set(e->bindings, (Symbol *)symbol("cons"), primitive(&prm_cons));
-    dict_set(e->bindings, (Symbol *)symbol("quit"), primitive(&prm_quit));
+    dict_set(e->bindings, (Symbol *)symbol("cons"), applicative(primitive(&prm_cons)));
+    dict_set(e->bindings, (Symbol *)symbol("quit"), applicative(primitive(&prm_quit)));
     for (;;) {
         fputs("> ", stdout);
         Object *result = eval(read(stdin), e);
